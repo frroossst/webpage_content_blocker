3a0749f873d9c83e1cfbed3469e8d3a4f1e2f3b8
home <adhyanpatel@gmail.com>
2022-05-25 11:09:41 -0600

    working MVP

--- blocker.js
+++ blocker.js
@@ -7,7 +7,9 @@ window.addEventListener("load", function() {
 
 function block_logic(){
 
-    const blocked_keywords = ["BLOCKED"];
+    const blocked_keywords = ["BLOCKED","blocked"];
+
+    console.log("looking for blocked keywords : ",blocked_keywords);
 
     let content_inner = document.documentElement.innerHTML;
     let content_outer = document.documentElement.outerHTML;
@@ -15,8 +17,10 @@ function block_logic(){
     let contains_blocked = false;
 
     for (let i = 0; i < blocked_keywords.length; i ++){
+        console.log(i);
         if (content_inner.includes(blocked_keywords[i])){
             contains_blocked = true;
+            console.log(contains_blocked[i]);
             break;
         }
         else if (content_outer.includes(blocked_keywords[i])){
@@ -24,10 +28,12 @@ function block_logic(){
             break;
         }
     }
+    console.log("reached here");
     
     //console.log(contains_blocked);
     
     if (contains_blocked == true || contains_blocked == 'true'){
+        console.log("loading blocked page");
         location.replace("blocked_page.html");       
     }
 }
